5feb6c9bb470bee059768f8ba3621e7e6628a466
justinscottjenecke <justinjenecke@gmail.com>
2024-09-18 12:17:28 +0200

    init: creates new web template spring app

---
+++ src/main/java/io/github/justinscottjenecke/calorietrackerbackend/CalorieTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.justinscottjenecke.calorietrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/justinscottjenecke/calorietrackerbackend/CalorieTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.justinscottjenecke.calorietrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
